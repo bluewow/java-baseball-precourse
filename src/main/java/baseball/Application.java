3054d9e7f840cc915c8292b5dee16f189f4c57f3
bluewow <dogseen@gmail.com>
2021-10-05 22:55:09 +0900

    feat : v2 (요구사항을 새로 분석하여 재작업)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,9 @@
 package baseball;
 
-import baseball.controller.GameController;
-import baseball.service.BaseballGame;
-import baseball.service.ComputerPlayer;
-import baseball.view.GameDisplay;
-import nextstep.utils.Console;
 
 public class Application {
 
     public static void main(String[] args) {
-        GameController gameController = new GameController();
-
-        gameController.play();
 
     }
 }
